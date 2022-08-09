8ee63f0d31dbb41f055eb51ed548e4dcc8bd21cb
Rishu Kumar <rishu110067@gmail.com>
2022-08-10 03:33:04 +0530

    link changes in exit-tryon:

--- public/components/exit-tryon.js
+++ public/components/exit-tryon.js
@@ -2,7 +2,7 @@ AFRAME.registerComponent('exit-tryon', {
     init: function () {
         let el = this.el
         this.exit = function () {
-            location.href = 'http://localhost:5000/metaverse'
+            location.href = 'https://spongeverse.herokuapp.com/metaverse'
         }
         this.el.addEventListener('click', this.exit)
     },
